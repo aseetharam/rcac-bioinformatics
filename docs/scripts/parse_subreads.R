88ce0bc3ae940173fc26a0791539d5d91f459e5f
Arun Seetharam <arnstrm@iastate.edu>
2024-10-26 13:31:27 -0500

    helixer

--- docs/scripts/parse_subreads.R
+++ docs/scripts/parse_subreads.R
@@ -2,23 +2,27 @@ library(data.table)
 library(tidyverse)
 setwd("C:/Users/Arun/Desktop/B73/test/")
 helixer_str <- fread("helixer.str2.summary")
-helixer_str_long <- pivot_longer(helixer_str, cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
+helixer_str_long <- pivot_longer(helixer_str,
+                                 cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
 helixer_str_long$strand <- "stranded"
 helixer_str_long$Annotation <- "helixer"
 
 helixer_unstr <- fread("helixer.unstr.summary")
-helixer_unstr_long <- pivot_longer(helixer_unstr, cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
+helixer_unstr_long <- pivot_longer(helixer_unstr,
+                                   cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
 helixer_unstr_long$strand <- "unstranded"
 helixer_unstr_long$Annotation <- "helixer"
 
 
 nam.v5_str <- fread("v5.str2.summary")
-nam.v5_str_long <- pivot_longer(nam.v5_str, cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
+nam.v5_str_long <- pivot_longer(nam.v5_str,
+                                cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
 nam.v5_str_long$strand <- "stranded"
 nam.v5_str_long$Annotation <- "NAM.v5"
 
 nam.v5_unstr <- fread("v5.unstr.summary")
-nam.v5_unstr_long <- pivot_longer(nam.v5_unstr, cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
+nam.v5_unstr_long <- pivot_longer(nam.v5_unstr,
+                                  cols = B73_16DAP_embryo_MN01101:B73_V18_tassel_MN01062)
 nam.v5_unstr_long$strand <- "unstranded"
 nam.v5_unstr_long$Annotation <- "NAM.v5"
 
@@ -27,27 +31,28 @@ myData <- rbind(helixer_str_long,
                 helixer_unstr_long,
                 nam.v5_str_long,
                 nam.v5_unstr_long)
-colnames(myData) <- c("fragment", "tissue", "count", "strand", "annotation")
+colnames(myData) <- c("fragment",
+                      "tissue",
+                      "count",
+                      "strand",
+                      "annotation")
 
 
 myData.filtered <- myData %>%
   filter(count  !=  0) %>%
   group_by(tissue, strand, annotation) %>%
-  mutate(total = sum(count), percent = (count / total) * 100)
+  mutate(total = sum(count),
+         percent = (count / total) * 100)
 
 myData.filtered.assigned <- myData.filtered %>%
-  filter(fragment  ==  "Assigned" & strand == "stranded")
+  filter(fragment  ==  "Assigned" &
+           strand == "stranded")
 
 myData.filtered.unassigned <- myData.filtered %>%
   filter(fragment  ==  "Unassigned_NoFeatures" &
            strand == "stranded")
 
 
-ggplot(myData.filtered, aes(x = tissue, y = percent, fill = fragment)) +
-  geom_bar(stat = "identity", position = "stack") +
-  labs(title = "Percentage of Counts per Status by Assignment Type", x = "Tissue", y = "Percentage (%)") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + facet_wrap(strand ~ annotation)
 
 status_colors <- c("helixer" = "#d79c3b", "NAM.v5" = "#a3c857")
 
@@ -78,10 +83,15 @@ ggplot(myData.filtered.assigned,
         legend.position = "top",
         legend.key = element_blank())
 
-ggsave("assigned.png",  width = 12, height = 10, units = "in", dpi = 300)
+ggsave("assigned.png",
+       width = 12,
+       height = 10,
+       units = "in",
+       dpi = 300)
 
 
-ggplot(myData.filtered.unassigned, aes(x = tissue, y = percent, fill = annotation)) +
+ggplot(myData.filtered.unassigned,
+       aes(x = tissue, y = percent, fill = annotation)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(title = "Unassigned Features",
        x = "Tissue",
@@ -106,4 +116,9 @@ ggplot(myData.filtered.unassigned, aes(x = tissue, y = percent, fill = annotatio
         legend.title = element_blank(),
         legend.position = "top",
         legend.key = element_blank())
-ggsave("unassigned.png", width = 12, height = 10, units = "in", dpi = 300)
+
+ggsave("unassigned.png",
+       width = 12,
+       height = 10,
+       units = "in",
+       dpi = 300)
